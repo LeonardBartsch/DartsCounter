e599ebb856ad46f9941c9489d126c7fcdf6fc6d9
Fabian Liebing <liebfabi@googlemail.com>
2019-06-27 17:21:51 +0200

    Startseite WIP

--- js/start.js
+++ js/start.js
@@ -3,3 +3,9 @@ const accountParam = "mitAccount";
 function weiterleiten(mitAccount){
     window.location.href = '../html/spielauswahl.html?' + accountParam + '=' + mitAccount;
 }
+
+function backtotop(){
+  window.scroll(0,0);
+}
+
+document.querySelector(".backtotopdiv").addEventListener("click",backtotop);
