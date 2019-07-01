69f24922a945d7a4b302dfd4ea08bb46b58fc52e
Fabian Liebing <liebfabi@googlemail.com>
2019-07-01 14:24:03 +0200

    WIP index + account

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ function backtotop(){
   window.scroll(0,0);
 }
 
+function comingsoon(){
+  window.alert("Coming soon");
+}
+
+
+document.querySelectorAll(".accountButton").forEach( x => x.addEventListener("click",comingsoon));
 document.querySelector(".backtotopdiv").addEventListener("click",backtotop);
